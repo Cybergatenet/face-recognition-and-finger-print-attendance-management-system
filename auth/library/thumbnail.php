7e6d98ffcd520ac2c25d777b08e19e5583982294
Cybergatenet <abelchinedu2@gmail.com>
2025-08-13 00:43:05 -0700

    fixed bugs

---
+++ auth/library/thumbnail.php
@@ -0,0 +1,31 @@
+
+<ul id="da-thumbs" class="da-thumbs">
+					<li>
+						<a href="">
+							<img src="images/MOUAU1.jpg" />
+							<div><span>Image 1</span></div>
+						</a>
+					</li>
+					
+					<li>
+						<a href="">
+							<img src="images/MOUAU2.jpg" />
+							<div><span>Image 2</span></div>
+						</a>
+					</li>
+					
+					<li>
+						<a href="">
+							<img src="images/MOUAU4.jpg" />
+							<div><span>Image 4</span></div>
+						</a>
+					</li>
+                    
+                    <li>
+						<a href="">
+							<img src="images/MOUAU3.jpg" />
+							<div><span>Image 4</span></div>
+						</a>
+					</li>
+</ul>
+					
